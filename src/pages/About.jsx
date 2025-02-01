447ca7bcc2329e67e65f77a471e49505b320c597
akinoqe <rvachef@icloud.com>
2025-02-01 19:41:03 +0300

    Добавил немного патриотизма

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1>О нас</h1>
+      <h1>СЛАВА РОССИИ</h1>
       <p>Это страница "О нас". Здесь можно рассказать о вашем сайте.</p>
     </div>
   );
